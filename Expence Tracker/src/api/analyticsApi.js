1aac48e4b5b7ab114a8cee9426c7c85d133b7d55
Sujal Sharma <sujalsharma484@gmail.com>
2026-09-07 11:50:26 +0530

    Delete Expence Tracker/src directory

--- Expence Tracker/src/api/analyticsApi.js
+++
@@ -1,5 +0,0 @@
-import API from './axios';
-
-export const getMonthlyAnalytics = () => API.get('/expenses/analytics/monthly');
-export const getCategoryAnalytics = (month, year) =>
-  API.get('/expenses/analytics/category', { params: { month, year } });
